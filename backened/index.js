b7b875f255fe6ba5545868d40b8bcba50fe10a6a
karunesc <karunesh.chhabra@nokia.com>
2020-09-28 20:52:17 +0530

    express code with logging

--- backened/index.js
+++ backened/index.js
@@ -4,9 +4,14 @@ const app =express();
 const port= 8000;
 
 app.get("/",(req,res)=>{
-  res.send("hello world")
+  res.send("Home page")
 
 });
+
+app.get("/logging",(req,res)=>{
+    res.send("You are visting logging route")
+  
+  });
 
 app.listen(port,()=>{
     
