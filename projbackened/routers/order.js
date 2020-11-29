f2788fec934f123d7112598d7e4d92a7dde3a43e
karunesc <karunesh.chhabra@nokia.com>
2020-11-29 17:08:23 +0530

    order router

--- projbackened/routers/order.js
+++ projbackened/routers/order.js
@@ -3,7 +3,7 @@ const router= express.Router();
 
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById,pushOrderInPurchaseList } = require("../controllers/user");
-const {getOrderById,createOrder,updateStatus,getAllOrders}= require("../controllers/order");
+const {getOrderById,createOrder,updateStatus,getAllOrders ,getOrderStatus}= require("../controllers/order");
 
 const {updateStock}=require("../controllers/product");
 
@@ -16,7 +16,7 @@ router.post("/order/create/:orderById/:userId", isSignedIn, isAuthenticated, pus
 router.put("/update/order/:orderById/status/:userId", isSignedIn, isAuthenticated, isAdmin, updateStatus);
 
 
-router.get("/order/status/:userId",isSignedIn,isAuthenticated,isAdmin,getAllOrders)
+router.get("/order/status/:userId",isSignedIn,isAuthenticated,isAdmin,getOrderStatus)
 
 router.get("/order/all/:userId",isSignedIn,isAuthenticated,isAdmin,getAllOrders)
 
